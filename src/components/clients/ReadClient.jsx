368117aff0b6afeafdc854318a2afc5c8a67e755
Ivan Bogdan <vanja.zeht2013@gmail.com>
2021-01-12 13:56:17 +0300

    city update

--- src/components/clients/ReadClient.jsx
+++ src/components/clients/ReadClient.jsx
@@ -1,25 +1,24 @@
 import React from "react";
+import "../style.css";
 
 const ReadClient = ({ onClose, client }) => {
   return (
     <div className="modal" id="id03">
       <form class="modal-content2 animate" style={{ padding: "0 5px" }}>
         <div class="imgcontainer">
-          <span
-            className="close"
-            onClick={onClose}
-            title="Close Modal"
-          >
+          <span className="close" onClick={onClose} title="Close Modal">
             ×
           </span>
           <p class="reg">Просмотр клиента</p>
         </div>
-        <div className="read_client">{client.name}</div>
-        <div className="read_client">{client.unp}</div>
-        <div className="read_client">{client.phone}</div>
-        <div className="read_client">{client.type}</div>
-        <div className="read_client">{client.city_id}</div>
-        <div className="read_client">{client.address}</div>
+        <div className="container3">
+          <div className="read_client">{client.name}</div>
+          <div className="read_client">{client.unp}</div>
+          <div className="read_client">{client.phone}</div>
+          <div className="read_client">{client.type}</div>
+          <div className="read_client">{client.city_id}</div>
+          <div className="read_client">{client.address}</div>
+        </div>
       </form>
     </div>
   );
