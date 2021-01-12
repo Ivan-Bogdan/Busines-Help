4aa0429d0fb3644cc1655ead72926e4a8ae37c20
Ivan Bogdan <vanja.zeht2013@gmail.com>
2021-01-12 13:20:00 +0300

    city update

--- src/components/clients/ReadClient.jsx
+++ src/components/clients/ReadClient.jsx
@@ -2,11 +2,23 @@ import React from "react";
 
 const ReadClient = ({ onClose, client }) => {
   return (
-    <div>
-      <div>{client.name}</div>
-      <div>{client.unp}</div>
-      <div>{client.phone}</div>
-      <div>{client.name}</div>
+    <div className="modal" id="id03">
+      <form class="modal-content2 animate" style={{ padding: "0 25px" }}>
+        <div class="imgcontainer">
+          <span
+            className="close"
+            onClick={onClose}
+            title="Close Modal"
+          >
+            ×
+          </span>
+          <p class="reg">Просмотр клиента</p>
+        </div>
+        <div>{client.name}</div>
+        <div>{client.unp}</div>
+        <div>{client.phone}</div>
+        <div>{client.name}</div>
+      </form>
     </div>
   );
 };
