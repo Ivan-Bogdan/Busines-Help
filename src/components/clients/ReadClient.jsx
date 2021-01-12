bcce260bcfc18255b7b78d282ce50f844d4b3f15
Ivan Bogdan <vanja.zeht2013@gmail.com>
2021-01-12 14:17:30 +0300

    city update

--- src/components/clients/ReadClient.jsx
+++ src/components/clients/ReadClient.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style.css";
 import "../Modal.css";
+import { city__name } from "../../API/http";
 
 const ReadClient = ({ onClose, client }) => {
+  const [city, setCity] = useState("");
+  useEffect(() => {
+    (async function () {
+      const result = await city__name({
+        id: client.city_id,
+      });
+      if (result.message) {
+        setCity("Unknown City");
+      } else {
+        const fullCity = result.type_abbr + ". " + result.city;
+        setCity(fullCity);
+      }
+    })();
+  }, [client]);
   return (
     <div className="modal" id="id03">
       <form class="modal-content2 animate" style={{ padding: "0 5px" }}>
@@ -17,8 +32,9 @@ const ReadClient = ({ onClose, client }) => {
           <div className="read_client">{client.unp}</div>
           <div className="read_client">{client.phone}</div>
           <div className="read_client">{client.type}</div>
-          <div className="read_client">{client.city_id}</div>
-          <div className="read_client">{client.address}</div>
+          <div className="read_client">
+            {city} {client.address}
+          </div>
         </div>
       </form>
     </div>
