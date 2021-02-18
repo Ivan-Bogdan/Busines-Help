260c1d2b49910bd7a6e3e4132008204b818dd656
Ivan Bogdan <vanja.zeht2013@gmail.com>
2021-02-18 11:23:50 +0300

    5215

--- src/components/MyClients.jsx
+++ src/components/MyClients.jsx
@@ -133,12 +133,11 @@ const MyClients = () => {
             </button>
             <button className="sorting"></button>
           </div>
-          {/* {clients.map((item, index) => (
+          {clients.map((item, index) => (
             <div key={index} className="client_item">
               <ClientItem item={item} deleteClient={deleteClient} />
             </div>
-          ))} */}
-          <TestClient></TestClient>
+          ))}
         </div>
       </section>
       <Footer />
