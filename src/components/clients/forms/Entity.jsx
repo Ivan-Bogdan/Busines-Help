4b6e3bf3da09619665e4e6266c2ca3dace129cf1
Your Name <vanja.zeht2013@gmail.com>
2021-05-16 14:16:58 +0300

    24

--- src/components/clients/forms/Entity.jsx
+++ src/components/clients/forms/Entity.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from "react";
 import { create_client } from "../../../API/http";
 import Additional from "./Additional";
-// import MaskedInput from "react-text-mask";
+import MaskedInput from "react-text-mask";
 
 const Entity = () => {
   const [unp, setUnp] = useState("");
@@ -77,7 +77,7 @@ const Entity = () => {
         onChange={({ target: { value } }) => setName(value)}
       />
       <p className="black">Телефон</p>
-      {/* <div>
+      <div>
         <MaskedInput
           type="text"
           value={phone}
@@ -103,8 +103,8 @@ const Entity = () => {
             /\d/,
           ]}
         />
-      </div> */}
-      <div className="form__field">
+      </div>
+      {/* <div className="form__field">
         <input
           type="text"
           pattern="(\+375|80|375)(29|25|44|33)(\d{3})(\d{2})(\d{2})"
@@ -115,7 +115,7 @@ const Entity = () => {
         <span className="form__error">
           Это поле содержит телефон в неверном формате
         </span>
-      </div>
+      </div> */}
       <p className="black">Примечание</p>
       <input
         type="text"
