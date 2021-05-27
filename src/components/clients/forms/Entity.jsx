6f777e095b345ad28d72a7d7c9bd5e54fd85dbaa
Your Name <vanja.zeht2013@gmail.com>
2021-05-27 11:17:47 +0300

    214

--- src/components/clients/forms/Entity.jsx
+++ src/components/clients/forms/Entity.jsx
@@ -65,6 +65,7 @@ const Entity = ({ client }) => {
 
   useEffect(() => {
     if (client) {
+      console.log(svedeniya.find((i) => i.label === "Адрес склада и др."));
       if (Boolean(client.reg_address)) {
         console.log(123);
         setSvedenia(
