79f281d44c58dbd63c31372b25229e156f5aa36d
Your Name <vanja.zeht2013@gmail.com>
2021-05-31 23:37:47 +0300

    21512

--- src/components/clients/forms/Entity.jsx
+++ src/components/clients/forms/Entity.jsx
@@ -153,10 +153,6 @@ const Entity = ({ client }) => {
     }
   }, [client, svedeniya]);
 
-  useEffect(() => {
-    if (clientCopy) setClientCopy({ ...clientCopy, ...addData });
-  }, [clientCopy, addData]);
-
   return (
     <div style={{ marginTop: 15 }}>
       <p className="black">УНП</p>
