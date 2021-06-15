4f0b52a2a4d0712470c253eb9a6fc537a3c1f2ff
Your Name <vanja.zeht2013@gmail.com>
2021-06-15 23:33:34 +0300

    124

--- src/components/tasks/UpdateTask.jsx
+++ src/components/tasks/UpdateTask.jsx
@@ -239,7 +239,7 @@ const UpdateTask = ({ task, FetchData, onClose }) => {
                     <RouteUpdate
                       data={item}
                       number={item.point}
-                      count={routes.length - 2}
+                      count={routes.length}
                       updateData={updateData}
                     />
                   ))}
