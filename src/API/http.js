0831556ee4266411fffa491ed88869f809b033a4
Your Name <vanja.zeht2013@gmail.com>
2021-06-25 16:21:47 +0300

    124

--- src/API/http.js
+++ src/API/http.js
@@ -7,10 +7,10 @@ const $api = axios.create({
     baseURL: API_URL
 })
 
-// $api.interceptors.request.use((config) => {
-//   config.headers.Authorization = `${localStorage.getItem('token')}`
-//   return config;
-// })
+$api.interceptors.request.use((config) => {
+  config.headers.Authorization = `${localStorage.getItem('token')}`
+  return config;
+})
 
 export const cityName = (payload) => {
 	return axios
