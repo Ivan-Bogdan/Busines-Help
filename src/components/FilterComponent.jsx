ef4358238f97aa9e69911b388d92cdf7f17a4deb
Ivan-B0gdan <ibogdan@remedypointsolutions.com>
2021-08-09 19:38:14 +0300

    124

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const FilterComponent = (filterList,) => {
+const FilterComponent = ({ filterList }) => {
   return (
     <div>
       {filterList.map((filterItem) => (
