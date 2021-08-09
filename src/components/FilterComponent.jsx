57fdcff19b4a39e863551d8f2dc7335e3ad1b8b2
Ivan-B0gdan <ibogdan@remedypointsolutions.com>
2021-08-09 21:36:17 +0300

    125

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const FilterComponent = ({ filterList, refetch, refetchSort, onClose }) => {
   const [sort, setSort] = useState('')
-  const [filters, setFilters] = useState(filterList)
+  const [filters, setFilters] = useState(filterList.map(item => { return { ...item, value: '' } }))
 
   const handleChange = (e, index) => {
     const newDoc = [...filters];
@@ -16,7 +16,7 @@ const FilterComponent = ({ filterList, refetch, refetchSort, onClose }) => {
         <div className='flex' >
           <input type='radio' value={filterItem.filter} name="radio" onChange={({ target }) => setSort(target.value)} checked={sort === filterItem.filter ? true : false}></input>
           <p className="ellips" style={{ padding: "0 10px", width: 250 }}> {filterItem.name}</p>
-          <input type='text' value={`${filterItem.filter}`} onChange={(e) => handleChange(e, index)} />
+          <input type='text' name="value" onChange={(e) => handleChange(e, index)} />
         </div>
       ))
       }
