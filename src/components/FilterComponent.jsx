a6f105b38a8e30e68b47bf29e55cf950987aade3
Ivan-B0gdan <ibogdan@remedypointsolutions.com>
2021-08-09 20:32:59 +0300

    124

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react';
 const FilterComponent = ({ filterList, refetch }) => {
   const [sort, setSort] = useState('')
   return (
-    <div>
+    <div style={{ padding: "0 10px" }}>
       {filterList.map((filterItem) => (
-        <div className='flex' style={{ padding: "0 10px" }}>
+        <div className='flex' >
           <input type='radio' value={filterItem.filter} name="radio" onChange={({ target }) => setSort(target.value)} checked={sort === filterItem.filter ? true : false}></input>
           <p className="ellips" style={{ padding: "0 10px" }}> {filterItem.name}</p>
           <input type='text'></input>
