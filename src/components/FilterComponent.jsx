8fdb156a3d8a985915e15f662f78b832fe40ed6c
Ivan-B0gdan <ibogdan@remedypointsolutions.com>
2021-08-11 15:35:51 +0300

    124

--- src/components/FilterComponent.jsx
+++ src/components/FilterComponent.jsx
@@ -72,7 +72,10 @@ const FilterComponent = ({ filterList, refetch, onClose }) => {
             suggestions={suggestions}
             onSuggestionsFetchRequested={onSuggestionsFetchRequested}
             onSuggestionsClearRequested={onSuggestionsClearRequested}
-            getSuggestionValue={(suggestion) => getSuggestionValue(suggestion, index)}
+            getSuggestionValue={(suggestion) => {
+              console.log(suggestion);
+              getSuggestionValue(suggestion, index)
+            }}
             renderSuggestion={renderSuggestion}
             inputProps={{
               placeholder: "Клиент",
