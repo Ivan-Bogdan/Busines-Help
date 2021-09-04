7345c8a3ee1d8392e0655b2cf0b3454d8a348f8d
Your Name <vanja.zeht2013@gmail.com>
2021-09-04 19:24:44 +0300

    124

--- src/components/MyServices.jsx
+++ src/components/MyServices.jsx
@@ -16,7 +16,7 @@ import { useLazyLoading } from "./hooks/useLazyLoading";
 
 const MyServ = () => {
 
-  const { setFetching } = useLazyLoading(containerBox, count, taskListFn, selectedTaskPage)
+
   const [tasks, setTasks] = useState([]);
   const [modal, setModal] = useState(false);
   const [isOpenFilter, setOpenFilter] = useState(false);
@@ -56,6 +56,7 @@ const MyServ = () => {
     },
     [sort, selectedTaskPage, tasks, resultFilter, isRefetch]
   )
+  const { setFetching } = useLazyLoading(containerBox, count, taskListFn, selectedTaskPage)
 
   const toggleModal = () => {
     setModal(!modal);
